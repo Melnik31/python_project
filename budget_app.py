0e860a1d5b8f4b49b83eaed3488eeb3374bbb9f5
Mike Melnikov <mikemelnikov@Mikes-MacBook-Air.local>
2024-09-09 17:32:55 -0500

    Data base was created. App was rescructured and ready to use.

---
+++ budget_app.py
@@ -0,0 +1,79 @@
+import db
+
+#db.add_one("Place", 99)
+
+#db.delete_one('5')
+
+'''record = [
+    ("Restoran", 51),
+    ("Shop", 12),
+    ("Other", 89),
+]
+'''
+#db.add_many(record)
+
+def menu():
+    print('\nMenu:')
+    print('\n1. Add transaction')
+    print('2. View transactions')
+    print('3. Delete transaction')
+    print('4. Exit')
+
+def add_trans():
+    print('\nEnter "0" to exit')
+    while True:
+        name = input('Enter the store or transaction name: ')
+        amount = input('Enter the amount: ')
+        if amount == '0':
+            break
+        db.add_one(name,amount)
+
+def view_transactions():
+    print('\nAll transactions: ')
+
+    db.show_all()
+
+
+def delete():
+    transactions = db.show_all()  # Ensure show_all() returns the transactions with rowid
+    existing_ids = [trans[0] for trans in transactions]  # Extract rowid values
+    
+    while True:
+        id_input = input('Enter the ID of transaction you want to delete.(Enter 0 to stop):')
+        if id_input == '0':
+            break
+        try:
+            id_input = int(id_input)
+            if id_input in existing_ids:
+                db.delete_one(id_input) 
+                print(f'Transaction with ID({id_input}) was deleted.')
+            else:
+                print('Invalid ID. ID does not exist')
+        except ValueError:
+            print('Invalid input. Enter the number.')
+    
+
+
+def main():
+
+    while True:
+
+        menu()
+
+        choice = input("Enter the number to use a menu: ")
+        
+        if choice == '1':
+            add_trans()
+        elif choice == '2':
+            view_transactions()
+        elif choice == '3':
+            delete()
+        elif choice == '4':
+            print('Exiting...')
+            break
+        else:
+            print('Invalid input')
+
+
+if __name__ == "__main__":
+    main()
---
+++ db.py
@@ -0,0 +1,62 @@
+import sqlite3
+
+
+
+# query the db and return all records
+def show_all():
+    #connect to db
+    con = sqlite3.connect('budget.db')
+    #create a courser
+    cur = con.cursor()  
+
+    #Query the database 
+    cur.execute("SELECT rowid, * FROM transactions")
+    items = cur.fetchall()
+
+    print(f'ID Store Price')
+    print(f'---------------')
+    for i in items:
+     print(f'{i[0]}: {i[1]} ${i[2]}')
+  
+    cur.execute("SELECT SUM(amount) FROM transactions")
+    total = cur.fetchone()
+    print(f'Total: ${total[0]}')
+
+    
+
+    #commit our command
+    con.commit()
+
+    #close our commit
+    con.close()
+    return items
+
+
+#add record to the table
+def add_one(name, amount):
+    con = sqlite3.connect('budget.db')
+    cur = con.cursor()  
+    cur.execute("INSERT INTO transactions VALUES (?,?)", (name, amount))
+    con.commit()
+    con.close()
+
+def add_many(list):
+    con = sqlite3.connect('budget.db')
+    cur = con.cursor()
+    
+    cur.executemany("INSERT INTO transactions VALUES (?,?)", (list))
+    
+    con.commit()
+    con.close()  
+
+def delete_one(id):
+    con = sqlite3.connect('budget.db')
+    cur = con.cursor() 
+    cur.execute("DELETE from transactions WHERE rowid = ?", (id,))
+
+    con.commit()
+    con.close()
+
+
+
+show_all()
